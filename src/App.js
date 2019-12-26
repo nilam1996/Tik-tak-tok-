7c0188e39164bd6b0fa1acea773abac99e7c762e
nilam1996 <nilam18@navgurukul.org>
2019-12-26 22:38:26 +0530

    Tik tak tok first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Shoping from "./componant/shoping"
+
+function App() {
+  return (
+    <div className="App">
+     <Shoping/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/componant/shoping.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+class ShopingList extends React.Component {
+    render() {
+        return (
+            <div className="shoping-list">
+                <h1>shoping list for </h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>Whatsapp</li>
+                    <li>Facebook</li>
+                </ul>
+            </div>
+        )
+
+    }
+}
+export default ShopingList;
